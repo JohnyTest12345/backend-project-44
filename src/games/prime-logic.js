715510b995e6ab327d42f5696fe06c60faf0d274
max <maksahater@gmail.com>
2023-09-02 20:46:33 +0500

    fix linter bugs

--- src/games/prime-logic.js
+++ src/games/prime-logic.js
@@ -1,19 +1,17 @@
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 function isPrime(number) {
-    for (let i = 2; i < number; i++) {
-      if (number % i === 0) return false;
-    }
-    return number !== 1;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) return false;
   }
-
+  return number !== 1;
+}
 
 function getTask() {
   const task = [];
   const number = getRandomInt(100);
 
   task[0] = number.toString();
-
 
   if (isPrime(number) === true) {
     task[1] = 'yes';

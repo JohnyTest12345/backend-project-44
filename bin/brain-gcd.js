d82ccabb6137e47db572991e8e25414cc3c58675
max <maksahater@gmail.com>
2023-09-03 16:13:44 +0500

    fix bug 2

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import getGame from '../src/index.js';
-import { getQuestion, getTask } from '../src/games/nod-logic.js';
+import { getQuestion, getTask } from '../src/games/gcd-logic.js';
 
 getGame(getQuestion(), getTask);
